e98a11370768c189dd6fe8bfcbb309b88cd0f2e5
Kaique Miranda <kaique.miranda@nuria.com.br>
2025-11-16 14:48:54 -0300

    fix: Add type annotations and docstring to __init__ method in AvaliacaoRestaurante class

--- components/avaliacao_restaurante.py
+++ components/avaliacao_restaurante.py
@@ -1,4 +1,12 @@
 class AvaliacaoRestaurante:
-    def __init__(self, media, avaliacoes_individuais):
+    """Classe que representa a avaliação de um restaurante.
+    """
+    def __init__(self, media: float, avaliacoes_individuais: list) -> None:
+        """Inicializa uma instância da classe AvaliacaoRestaurante.
+
+        Args:
+            media (float): Média das avaliações do restaurante.
+            avaliacoes_individuais (list): Lista de avaliações individuais do restaurante.
+        """
         self.media = media
         self.avaliacoes_individuais = avaliacoes_individuais
