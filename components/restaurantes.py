32aaa31599742b262cfb55d2ac6cdd073bf6cc4d
Kaique Miranda <kaique.miranda@nuria.com.br>
2025-11-16 14:49:16 -0300

    fix: Add type annotations and docstrings to __init__ and obter_restaurantes methods in Restaurantes class for improved clarity

--- components/restaurantes.py
+++ components/restaurantes.py
@@ -2,10 +2,25 @@ from .restaurante import Restaurante
 
 
 class Restaurantes:
-    def __init__(self, restaurantes_data):
+    """Representa uma coleção de restaurantes.
+    """
+    def __init__(self, restaurantes_data: dict) -> None:
+        """Inicializa uma instância da classe Restaurantes.
+
+        Args:
+            restaurantes_data (dict): Dados dos restaurantes.
+        """
         self._lista_de_restaurantes = self.obter_restaurantes(restaurantes_data)
-    
-    def obter_restaurantes(self, restaurantes_data):
+
+    def obter_restaurantes(self, restaurantes_data: dict) -> list[Restaurante]:
+        """Retorna a lista de restaurantes a partir dos dados fornecidos.
+
+        Args:
+            restaurantes_data (dict): Dados dos restaurantes.
+
+        Returns:
+            list[Restaurante]: Lista de objetos Restaurante.
+        """
         lista_de_restaurantes = []
         for restaurante in restaurantes_data["restaurants"]:
             lista_de_restaurantes.append(
