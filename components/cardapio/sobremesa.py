a1d7cdace6d7a696a50d91a01f23b658a59c4a07
Kaique Miranda <kaique.miranda@nuria.com.br>
2025-11-16 14:50:05 -0300

    fix: Add type annotations and docstrings to methods in Sobremesa class for improved clarity

--- components/cardapio/sobremesa.py
+++ components/cardapio/sobremesa.py
@@ -1,10 +1,23 @@
 from .item_cardapio import ItemCardapio
 
+
 class Sobremesa(ItemCardapio):
-    def __init__(self, nome, preco, tipo, tamanho):
+    """Classe que representa um item do tipo Sobremesa.
+    """
+    def __init__(self, nome: str, preco: float, tipo: str, tamanho: str) -> None:
+        """Inicializa um item do tipo Sobremesa.
+
+        Args:
+            nome (str): Nome da sobremesa.
+            preco (float): Preço da sobremesa.
+            tipo (str): Tipo da sobremesa.
+            tamanho (str): Tamanho da sobremesa.
+        """
         super().__init__(nome, preco)
         self.tipo = tipo
         self.tamanho = tamanho
-    
-    def aplicar_desconto(self):
+
+    def aplicar_desconto(self) -> None:
+        """Aplica um desconto de 15% no preço da sobremesa.
+        """
         self._preco -= (self._preco * 0.15)
