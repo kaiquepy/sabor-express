0fe06cd7671cd68a635a95decc0b8b6fe2d94529
Kaique Miranda <kaique.miranda@nuria.com.br>
2025-11-16 14:49:40 -0300

    fix: Add type annotations and docstrings to methods in ItemCardapio class for improved clarity

--- components/cardapio/item_cardapio.py
+++ components/cardapio/item_cardapio.py
@@ -1,13 +1,29 @@
 from abc import ABC, abstractmethod
 
+
 class ItemCardapio(ABC):
-    def __init__(self, nome, preco):
+    """Classe abstrata que representa um item do cardápio.
+    """
+    def __init__(self, nome: str, preco: float) -> None:
+        """Inicializa um item do cardápio.
+
+        Args:
+            nome (str): Nome do item.
+            preco (float): Preço do item.
+        """
         self._nome = nome
         self._preco = preco
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Retorna a representação em string do item do cardápio.
+
+        Returns:
+            str: Nome do item.
+        """
         return f'{self._nome}'
 
     @abstractmethod
-    def aplicar_desconto(self):
+    def aplicar_desconto(self) -> None:
+        """Aplica um desconto no preço do item do cardápio.
+        """
         pass
