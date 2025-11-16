cd944f22617a96d59d5725b0a5e521477d50dc57
Kaique Miranda <kaique.miranda@nuria.com.br>
2025-11-16 14:49:58 -0300

    fix: Add type annotations and docstrings to methods in Prato class for improved clarity

--- components/cardapio/prato.py
+++ components/cardapio/prato.py
@@ -1,9 +1,21 @@
 from .item_cardapio import ItemCardapio
 
+
 class Prato(ItemCardapio):
-    def __init__(self, nome, preco, descricao):
+    """Classe que representa um item do tipo Prato.
+    """
+    def __init__(self, nome: str, preco: float, descricao: str) -> None:
+        """Inicializa um item do tipo Prato.
+
+        Args:
+            nome (str): Nome do prato.
+            preco (float): Preço do prato.
+            descricao (str): Descrição do prato.
+        """
         super().__init__(nome, preco)
         self.descricao = descricao
-    
-    def aplicar_desconto(self):
+
+    def aplicar_desconto(self) -> None:
+        """Aplica um desconto de 5% no preço do prato.
+        """
         self._preco -= (self._preco * 0.05)
