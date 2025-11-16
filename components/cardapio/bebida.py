4fe0ccdc85351ecef3e3d4abef39de834fabcbfb
Kaique Miranda <kaique.miranda@nuria.com.br>
2025-11-16 14:49:45 -0300

    fix: Add type annotations and docstrings to methods in Bebida class for improved clarity

--- components/cardapio/bebida.py
+++ components/cardapio/bebida.py
@@ -1,9 +1,21 @@
 from .item_cardapio import ItemCardapio
 
+
 class Bebida(ItemCardapio):
-    def __init__(self, nome, preco, tamanho):
+    """Classe que representa um item do tipo Bebida.
+    """
+    def __init__(self, nome: str, preco: float, tamanho: str) -> None:
+        """Inicializa um item do tipo Bebida.
+
+        Args:
+            nome (str): Nome da bebida.
+            preco (float): Preço da bebida.
+            tamanho (str): Tamanho da bebida.
+        """
         super().__init__(nome, preco)
         self.tamanho = tamanho
-    
-    def aplicar_desconto(self):
+
+    def aplicar_desconto(self) -> None:
+        """Aplica um desconto de 8% no preço da bebida.
+        """
         self._preco -= (self._preco * 0.08)
